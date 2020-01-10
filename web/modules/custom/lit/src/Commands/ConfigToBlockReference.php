8108d7579027d626d4890122412cce17c2349d29
Martin Yde Granath <my@aarhus.dk>
2020-01-10 14:09:03 +0100

    LIT-225: Changed templates

--- web/modules/custom/lit/src/Commands/ConfigToBlockReference.php
+++ web/modules/custom/lit/src/Commands/ConfigToBlockReference.php
@@ -89,7 +89,7 @@ class ConfigToBlockReference extends DrushCommands {
         if ($blockConfig['id'] == 'views_block__book_list_books_block') {
           $this->deleteConfig($blockConfig);
         }
-        if ($blockConfig['id'] == 'views_block__related_books_related_books_by_author_block') {
+        if ($blockConfig['id'] == 'views_block__related_books_by_author_block') {
           $this->deleteConfig($blockConfig);
         }
       }
