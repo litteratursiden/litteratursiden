d1c5da4f377100f4c461b277d0b398a260a33e3a
martinyde <yde001@gmail.com>
2022-02-04 09:31:15 +0100

    - Moved ignore rule to theme - Removed gulp from theme and implemented webpack

---
+++ web/themes/custom/litteratursiden/js/app.js
@@ -0,0 +1,13 @@
+import "../sass/style.scss";
+
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+
+// Import Light icons
+import {} from "@fortawesome/pro-light-svg-icons";
+
+// Import Brand icons
+import {} from "@fortawesome/free-brands-svg-icons";
+
+// Add light and brand icons
+library.add();
+dom.watch();
