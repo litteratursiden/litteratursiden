f8642d17b00f039a9f6939c07d5c2ad802ce8667
martinyde <yde001@gmail.com>
2023-05-11 13:33:12 +0200

    Changed styling for dropdown

--- web/themes/custom/litteratursiden/js/dropdown-list.js
+++ web/themes/custom/litteratursiden/js/dropdown-list.js
@@ -3,6 +3,7 @@ let elements = document.querySelectorAll(".custom-accordion");
 
 [...elements].forEach(el => {
     el.addEventListener("click", function(e) {
+        console.log('123');
         this.classList.toggle("ui-accordion-header-active");
         this.classList.toggle("ui-state-active");
     }, false);
