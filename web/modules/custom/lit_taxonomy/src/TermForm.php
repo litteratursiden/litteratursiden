bb18828b101a5f137aebce3d3c651a32b067ad43
martinyde <yde001@gmail.com>
2023-08-28 09:12:48 +0200

    Changed term form

--- web/modules/custom/lit_taxonomy/src/TermForm.php
+++ web/modules/custom/lit_taxonomy/src/TermForm.php
@@ -2,11 +2,9 @@
 
 namespace Drupal\lit_taxonomy;
 
-use Drupal\taxonomy\TermForm as BaseTermForm;
-
 /**
  * Base for handler for taxonomy term edit forms.
  */
-class TermForm extends BaseTermForm {
+class TermForm {
 
 }
